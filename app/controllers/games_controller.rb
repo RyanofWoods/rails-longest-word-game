7835ae679c87f7ed63c9c6a2de80d589c563cbcd
Ryan Woods <contact@ryanofwoods.com>
2021-02-15 16:30:24 +0100

    Added score with including time taken to guess. Next: add a grand score saved over the session

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,13 +6,17 @@ class GamesController < ApplicationController
     @letters = []
     # create 10 random letters
     10.times { @letters << ('A'..'Z').to_a[rand(26)] }
+    @time_start = Time.now.to_i
   end
   def score
-    @letters = params[:letters].split()
-    @answer = params[:answer].upcase
+    letters = params[:letters].split()
+    answer = params[:answer].upcase
+    time_start = params[:time_start].to_i
+    seconds_taken = Time.now.to_i - time_start
+    check_letters = letters.clone
+    check_answer = answer.chars
+    score = 0
     @results = []
-    check_letters = @letters.clone
-    check_answer = @answer.chars
 
     # preinitialise
     all_included = true
@@ -26,25 +30,34 @@ class GamesController < ApplicationController
         index = check_letters.index(letter)
         check_letters.delete_at(index)
       else
-        @results << "#{@answer} can't be built out of #{@letters.join(", ")}"
+        @results << "#{answer} can't be built out of #{letters.join(", ")}"
         all_included = false
         break
       end
     end
 
     # next: check if it is an english word
-    url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
+    url = "https://wagon-dictionary.herokuapp.com/#{answer}"
     word_serialized = open(url).read
     word_result = JSON.parse(word_serialized)
 
     in_dictionary = word_result["found"]
 
     unless in_dictionary
-      @results << "#{@answer} does not seem to be a valid English word..."
+      @results << "#{answer} does not seem to be a valid English word..."
     end
 
     if in_dictionary && all_included
-      @results << "Congratulations! #{@answer} is a valid English word!"
+      if seconds_taken > 30
+        @results << "Your word was valid, but you took more than 30 seconds..."
+      else
+        score = answer.length + (30 / seconds_taken)
+
+        @results << "Congratulations! #{answer} is a valid English word!"
+        @results << "You took #{seconds_taken} seconds to guess"
+        @results << "You just got: #{score} points!"
+        #@results << "Your grand score:"
+      end
     end
   end
 end
