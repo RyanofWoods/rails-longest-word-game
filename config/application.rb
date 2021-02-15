f2b7fe6625f6942b7af87664b076340b0517dca9
Ryan Woods <contact@ryanofwoods.com>
2021-02-15 17:58:32 +0100

    Added styling and autofocus to elements so the user doesnt need to use the mouse (better ux and faster times)

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,5 @@ module RailsLongestWordGame
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    Rails.application.config.session_store :cookie_store, key: '_your_app_session'
   end
 end
