85e7cc96999e0b6642df6436946bd8be89979b9d
Ryan Woods <contact@ryanofwoods.com>
2021-02-15 14:50:59 +0100

    Added random letter cards on new page with styling, form next

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,8 @@
 class GamesController < ApplicationController
   def new
+    @letters = []
+    # create 10 random letters
+    10.times { @letters << ('A'..'Z').to_a[rand(26)] }
   end
   def score
   end
